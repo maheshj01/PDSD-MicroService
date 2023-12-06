80af29ce61a6ec5b863cd22e14522a72e724f02c
mahesh jamdade <maheshmn121@gmail.com>
2023-12-05 22:31:04 -0500

    buffer overflow

--- checkout_service/src/services/checkoutService.ts
+++ checkout_service/src/services/checkoutService.ts
@@ -14,11 +14,24 @@ export class CheckoutService {
     return result.rows.length > 0 ? result.rows[0] : null;
   }
 
+  // Updated to use safe string handling
   public async addNewCheckout(user_id: number, book_id: number, due_date: Date): Promise<Checkout> {
+    // Basic input validation
+    if (!user_id || !book_id || !due_date) {
+      throw new Error('Invalid input parameters');
+    }
+
+    // Using parameterized queries to prevent SQL injection
     const result = await pool.query(
       'INSERT INTO checkouts (user_id, book_id, due_date) VALUES ($1, $2, $3) RETURNING *',
       [user_id, book_id, due_date]
     );
-    return result.rows[0];
+
+    // Ensure the result.rows[0] is not null before returning
+    if (result.rows.length > 0) {
+      return result.rows[0];
+    } else {
+      throw new Error('Failed to add new checkout');
+    }
   }
 }

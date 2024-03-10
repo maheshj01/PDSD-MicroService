881a07f1e55087e2e210b167d2c7e1ed41e82b34
mahesh jamdade <maheshmn121@gmail.com>
2024-03-10 17:25:38 -0400

    Add Login Refirect

---
+++ library-web-app/src/components/AppWrapper.tsx
@@ -0,0 +1,31 @@
+// src/components/AppWrapper.tsx
+
+import React, { useEffect } from "react";
+import { BrowserRouterProps, useNavigate } from "react-router-dom";
+
+const AppWrapper: React.FC<BrowserRouterProps> = ({ children }) => {
+    const navigate = useNavigate();
+
+    const isAuthenticated = () => {
+        // Check if the token is present and valid (you might need additional checks)
+        const token = localStorage.getItem("token");
+        return token !== null && token !== undefined;
+    };
+
+    useEffect(() => {
+        console.log("AppWrapper mounted");
+
+        // Redirect logic if needed on component mount
+        if (!isAuthenticated()) {
+            // Redirect to login if not authenticated
+            navigate("/login");
+        } else {
+            // Redirect to dashboard if authenticated
+            navigate("/dashboard");
+        }
+    }, [navigate]);
+
+    return <>{children}</>;
+};
+
+export default AppWrapper;
--- library-web-app/src/index.tsx
+++ library-web-app/src/index.tsx
@@ -1,10 +1,11 @@
+// src/index.tsx
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppWrapper from "./components/AppWrapper";
 import Home from "./components/Home";
 import Login from "./components/Login";
-
-// Load environment variables
 
 const root = document.getElementById("root");
 
@@ -12,12 +13,13 @@ if (root) {
   const reactRoot = createRoot(root);
   reactRoot.render(
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Home />} />
-      </Routes>
+      <AppWrapper>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Home />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </AppWrapper>
     </Router>
   );
 }
-
-
